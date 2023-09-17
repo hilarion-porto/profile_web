e080880f635467ae47ba69ce692ce4d3b7498390
hilarion <hilarion@hilarions-MacBook-Air.local>
2023-09-17 22:43:22 +0700

    add landing page

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,5 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
-app.component('MultiSplitPane')
-app.component('Pane')
 
 app.mount('#app')
---
+++ src/views/HomePageView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// import { defineComponent } from 'vue';
+import TheWelcome from '../components/TheWelcome.vue'
+import Header from '@/components/header/Header.vue';
+import AboutMe from '@/components/about/AboutMe.vue';
+// import Project from '@/components/project/Project.vue';
+import Contact from '@/components/contact/Contact.vue';
+// import { defineComponent } from 'vue/dist/vue.js';
+
+// export default defineComponent({
+//   components: {
+//     Header,
+//     AboutMe,
+//     Project,
+//     Contact,
+//   },
+// });
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <!-- <about /> -->
+    <!-- <AboutMe />
+    <Project />
+    <Contact /> -->
+  </main>
+</template>
